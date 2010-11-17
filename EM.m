50c820d97bf715cae776da84c5aedc6d1e682a39
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2010-11-17 16:40:03 +0100

    partial MMLE EM algorithm implementation

---
+++ EM.m
@@ -0,0 +1,26 @@
+function [ output_args ] = EM( x )
+%EM Summary of this function goes here
+%   Detailed explanation goes here
+
+max_steps = 10;
+n = size(x, 1)
+dim = size(x, 2)
+
+%% Iteration Start Values
+mu = x;
+pi_j = ones(size(x, 1), 1);
+
+%% EM Iteration
+for step=1:max_steps
+    %% E-step
+    pi_ij=zeros(n);
+    for j=1:n
+        pi_ij(:,j) = pi_j(j) * gauss(x, repmat(mu(j,:),n,1), eye(dim)); % TODO use real sigma instead of eye
+    end
+    pi_ij = pi_ij ./ repmat(sum(pi_ij, 2),1,n);
+    
+    %...
+    %% M-step
+end
+
+end
