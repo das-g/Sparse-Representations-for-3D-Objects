594b3e2e41df166a13c34f549e11fd8ea9b5e8e0
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2010-11-19 11:59:41 +0100

    make gauss function non-verbose

--- gauss.m
+++ gauss.m
@@ -1,6 +1,6 @@
 function [ phi ] = gauss( x, mu, Sigma)
 % Multivariate Gauss distribution
-dim = size(x,2)
+dim = size(x,2);
 phi =  (2 * pi) ^ (-dim/2) * 1 / sqrt(norm(Sigma)) ...
        * exp(-0.5 * dot((x - mu) / Sigma, (x - mu), 2) );
 end
