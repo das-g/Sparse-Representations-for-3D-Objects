2810065ffc6838c93837206336e71a067557a6f8
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2010-11-19 12:00:53 +0100

    variable SIGMA matrix

--- EM.m
+++ EM.m
@@ -9,13 +9,15 @@ dim = size(x, 2)
 %% Iteration Start Values
 mu = x;
 pi_j = ones(size(x, 1), 1);
+sigma = 1;
+SIGMA = repmat(eye(dim),[1 1 n]) * sigma^2;
 
 %% EM Iteration
 for step=1:max_steps
     %% E-step
     pi_ij=zeros(n);
     for j=1:n
-        pi_ij(:,j) = pi_j(j) * gauss(x, repmat(mu(j,:),n,1), eye(dim)); % TODO use real sigma instead of eye
+        pi_ij(:,j) = pi_j(j) * gauss(x, repmat(mu(j,:),n,1), SIGMA(:,:,j));
     end
     pi_ij = pi_ij ./ repmat(sum(pi_ij, 2),1,n);
     
