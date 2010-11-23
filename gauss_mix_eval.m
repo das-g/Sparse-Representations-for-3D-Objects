9b1084692c76c89d6fd11def8d2f8ae9871084e7
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2010-11-23 15:43:52 +0100

    allow evaluating gaussian mixture at several points at once

--- gauss_mix_eval.m
+++ gauss_mix_eval.m
@@ -10,13 +10,15 @@ function [ result ] = gauss_mix_eval( mu, SIGMA, x )
 %       SIGMA   is a p-by-d-by-d array where SIGMA(j,:,:) is the d-by-d
 %               covariance matrix corresponding to the j-th center
 %
-%       x       1-by-d position where the function is evaluated
+%       x       n-by-d array where each of the n rows represents a position
+%               where the function shall be evaluated
 
-p = size(mu,1);
+[p, dim] = size(mu);
+n = size(x,1);
 
 result = 0;
 for j=1:p
-    result = result + gauss(x, mu(j,:), squeeze(SIGMA(j,:,:)));
+    result = result + gauss(x, repmat(mu(j,:), [n 1]), squeeze(SIGMA(j,:,:)));
 end
 
 end
