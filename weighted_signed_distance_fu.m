7b298bcea7d5b2d4f86e2cc66ac61d49f29d76f5
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2010-12-05 19:41:15 +0100

    signed distance fuction, weighted with gauss kernels

---
+++ weighted_signed_distance_fu.m
@@ -0,0 +1,35 @@
+function [ f ] = weighted_signed_distance_fu( mu, normals, SIGMA, x )
+%Compute the weighted signed distance function (with gauss kernels)
+%   Compute the weighted signed distance function (for gauss kernels at
+%   centers mu with covariance matrices SIGMA) at position x
+%
+%       mu      is a p-by-d matrix where each of the p rows represents the
+%               (d-dimensional) position of a center
+%
+%       normals is a p-by-d matrix where each of the p rows represents the
+%               (d-dimensional) directed normal of the zero-isosurface
+%               at a center
+%
+%       SIGMA   is a p-by-d-by-d array where SIGMA(j,:,:) is the d-by-d
+%               covariance matrix corresponding to the j-th center
+%
+%       x       n-by-d array where each of the n rows represents a position
+%               where the function shall be evaluated
+
+p = size(mu,1);
+n = size(x,1);
+
+numerator = 0;
+denominator = 0; % for normalization
+
+for j=1:p
+    kernel_value = gauss(x, repmat(mu(j,:), [n 1]), squeeze(SIGMA(j,:,:)));
+    numerator = numerator + dot(repmat(normals(j,:), [n 1]), ...
+                                x - repmat(mu(j,:), [n 1]), ...
+                                2) .* kernel_value;
+    denominator = denominator + kernel_value;
+end
+
+f = numerator ./ denominator;
+
+end
