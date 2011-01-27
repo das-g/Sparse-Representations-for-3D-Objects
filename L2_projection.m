bdcd7a223b8a4303e059bd3c828be916fbfa70b3
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-01-27 15:38:34 +0100

    distinguish between measurement points and points where we reconstruct the function

--- L2_projection.m
+++ L2_projection.m
@@ -1,29 +1,40 @@
-x = (0:0.01:5)';
+x_measurement = (0:0.01:5)';
+x_reconstruct = (0:0.01:5)';
 mu =(0:0.1:5)';
 SIGMA = 1;
 
 p = size(mu,1);
-n = size(x,1);
+n_measurement = size(x_measurement, 1);
+n_reconstruct = size(x_reconstruct, 1);
 
-K = zeros(n,p);
+K_measurement = zeros(n_measurement, p);
+K_reconstruct = zeros(n_reconstruct, p);
 
 for j=1:p
-    K(:,j) = gauss(x, repmat(mu(j,:), [n 1]), squeeze(SIGMA));
+    K_measurement(:,j) = gauss(x_measurement, ...
+                               repmat(mu(j,:), [n_measurement 1]), ...
+                               squeeze(SIGMA));
+    K_reconstruct(:,j) = gauss(x_reconstruct, ...
+                               repmat(mu(j,:), [n_reconstruct 1]), ...
+                               squeeze(SIGMA));
 end
 
-f = cos(7*x) + sin(5*x);
+f = @(x) cos(7*x) + sin(5*x);
 
-alpha = K\f;
+f_measured = f(x_measurement);
 
-f_reconstructed = K * alpha;
+alpha = K_measurement \ f_measured;
+
+f_reconstructed = K_reconstruct * alpha;
+f_original = f(x_reconstruct); % actual value of f at reconstruction points
 
 subplot(2,1,1)
-plot(x,f)
+plot(x_reconstruct, f_original)
 hold on
-plot(x,f_reconstructed,'color','red')
+plot(x_reconstruct, f_reconstructed, 'color', 'red')
 legend('f','reconstructed f')
 hold off
 
 subplot(2,1,2)
-plot(x,f_reconstructed - f)
+plot(x_reconstruct, f_reconstructed - f_original)
 title('error')
