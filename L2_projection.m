310051418309257d1303faf11ddfbf130aec5d6b
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-02-02 16:41:10 +0100

    Added approximation looking for minimal ||alpha||_L1 under equality condition (K * alpha == f)

--- L2_projection.m
+++ L2_projection.m
@@ -9,6 +9,13 @@ x_reconstruct = (x_min:0.01:x_max)';
 mu = (x_min:1:x_max)';
 SIGMA = 4;
 
+%% prepare search path
+% save path before we manipulate it (for restoring it later)
+old_path = path;
+
+% addpath want absolute paths, so prepend pwd/
+addpath([pwd '/../l1magic-1.1/Optimization'])
+
 %% build measurement and reconstruction matrix
 p = size(mu, 1);
 n_measurement = size(x_measurement, 1);
@@ -29,10 +36,15 @@ end
 %% measure f
 f_measured = f(x_measurement);
 
-alpha = K_measurement \ f_measured;
+% L2 approximation also used as start point for L1 iterations
+alpha_L2 = K_measurement \ f_measured;
+
+alpha_L1eq = l1eq_pd(alpha_L2, K_measurement, [], f_measured);
 
 %% reconstruct f
-f_reconstructed = K_reconstruct * alpha;
+f_reconstructed_L2 = K_reconstruct * alpha_L2;
+f_reconstructed_L1eq = K_reconstruct * alpha_L1eq;
+
 %% measure f at reconstruction points (for plotting & comparison)
 f_original = f(x_reconstruct); % actual value of f at reconstruction points
 
@@ -40,11 +52,20 @@ f_original = f(x_reconstruct); % actual value of f at reconstruction points
 subplot(2, 1, 1)
 plot(x_reconstruct, f_original)
 hold on
-plot(x_reconstruct, f_reconstructed, 'color', 'red')
-legend('f','reconstructed f')
+plot(x_reconstruct, f_reconstructed_L2, 'color', 'red')
+plot(x_reconstruct, f_reconstructed_L1eq, 'color', 'green')
+legend('f', ...
+       'reconstructed f (from L2)', ...
+       'reconstructed f (from L1 with eq. cond.)')
 hold off
 
 %% Plot error in lower half
 subplot(2, 1, 2)
-plot(x_reconstruct, f_reconstructed - f_original)
+plot(x_reconstruct, f_reconstructed_L2 - f_original, 'color', 'red')
+hold on
+plot(x_reconstruct, f_reconstructed_L1eq - f_original, 'color', 'green')
+hold off
 title('error')
+
+%% restore old path
+path(old_path)
