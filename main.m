2d1b67a67c15c7360ee3637d0275d7c3f3588946
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-02-14 14:40:31 +0100

    corrected measurement matrix computation

--- main.m
+++ main.m
@@ -47,6 +47,8 @@ goal_sigma = 130;
 % NOT the other waz around. Else only the nearest k measurement points
 % would 'see' a given kernel.
 Xq = x; % query points, use input points for now
+%mu = x; % reference points, use input points for now
+mu_normals = x_normals; % normals at reference points (i.e. at kernel centers)
 
 old_path = path;
 addpath([pwd '/../ann_mwrapper'])
@@ -61,15 +63,21 @@ p = size(mu,1);
 A = sparse(n,p);
 
 for i = 1:n
-    neighborMuIdx = nnidx(:, i);
+    neighborMuIndices = nnidx(:, i);
     % The i'th measurement point might have less than size(nnidx, 1)
     % neighbors. Eliminate empty idx entires
-    neighborMuIdx = neighborMuIdx(neighborMuIdx > 0);
-    numNeighbors = length(neighborMuIdx);
+    neighborMuIndices = neighborMuIndices(neighborMuIndices > 0);
+    numNeighbors = length(neighborMuIndices);
     
-    % FIXME: weighted_signed_distance_fu isn't the right thing to call
-    % here: We need separate values per kernel, not the sum.
-    A(i, neighborMuIdx) = weighted_signed_distance_fu(mu(neighborMuIdx,:), x_normals, ...
-                                                      reshape(SIGMA(:,neighborMuIdx,:,:), [numNeighbors dim dim]), ...
-                                                      Xq(i,:));
+    numerator = 0;
+    denominator = 0; % for normalization
+    
+    for j=neighborMuIndices'
+        kernel_value = gauss(Xq(i, :), mu(j, :), reshape(SIGMA(:,j,:,:), [dim dim]));
+        A(i,j) = dot(mu_normals(j,:), ...
+                                Xq(i,:) - mu(j,:), ...
+                                2) * kernel_value;
+        denominator = denominator + kernel_value;
+    end
+    A(i, neighborMuIndices) = A(i, neighborMuIndices) ./ denominator;
 end
