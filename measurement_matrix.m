9f59195c3c64955953c450619e3545dccf4bb7b0
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-02-23 15:58:55 +0100

    iterate over kernel centers rather than measurement points when constructing measurement matrix

--- measurement_matrix.m
+++ measurement_matrix.m
@@ -3,26 +3,30 @@ function A = measurement_matrix(mu, normals, SIGMA, x)
 old_path = path;
 addpath([pwd '/../ann_mwrapper/extension'])
 
-[mu_kdtree, pts] = annConstructTree(mu');
+[x_kdtree, pts] = annConstructTree(x');
 
 [n dim] = size(x);
 p = size(mu,1);
 
 A = sparse(n,p);
 
-for i = 1:n
-    neighborMuIndices = annFindKnn(mu_kdtree, x(i, :)', 10, 0, 0);
+for j = 1:p
+    circumcircle_radius = max(eig(reshape(SIGMA(:,j,:,:), [dim dim])));
+    neighborXIndices = annFindKnn(x_kdtree, mu(j, :)', ...
+                                  circumcircle_radius ^ 2 * 1e1, ... % FIXME: max. number of neighbors should also depend on the density of x
+                                  circumcircle_radius, 0);
     % The i'th measurement point might have less than size(nnidx, 1)
     % neighbors. Eliminate empty idx entires
-    neighborMuIndices = neighborMuIndices(neighborMuIndices > 0);
+    neighborXIndices = neighborXIndices(neighborXIndices > 0);
+    numXNeighbors = size(neighborXIndices,1);
     
-    for j=neighborMuIndices'
-        kernel_value = gauss(x(i, :), mu(j, :), reshape(SIGMA(:,j,:,:), [dim dim]));
-        A(i,j) = dot(normals(j,:), ...
-                                x(i,:) - mu(j,:), ...
-                                2) * kernel_value;
-    end
+    kernel_value = gauss(x(neighborXIndices, :), ...
+                         repmat(mu(j, :), [numXNeighbors 1]), ...
+                         reshape(SIGMA(:,j,:,:), [dim dim]));
+    A(neighborXIndices,j) = dot(repmat(normals(j, :), [numXNeighbors 1]), ...
+                            x(neighborXIndices, :) - repmat(mu(j, :), [numXNeighbors 1]), ...
+                            2) .* kernel_value;
 end
 
-annDeleteTree(mu_kdtree, pts)
+annDeleteTree(x_kdtree, pts)
 path(old_path)
