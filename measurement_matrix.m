85c068de8368600820be7404c46ee1c0ff0baa4c
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-03-06 16:32:46 +0100

    compile measurement_matrix_values with emlmex "at runtime"

--- measurement_matrix.m
+++ measurement_matrix.m
@@ -13,9 +13,15 @@ p_nn = size(nnidx, 1);
 
 A_i = repmat((1:n)', [1 p_nn]);
 A_j = double(nnidx');
+
+% The sizes of passed parameters are not known in advance, so we compile
+% measurement_matrix_values just before calling it. For large n or p_nn
+% this still pays off.
+emlmex measurement_matrix_values -eg {mu, normals, SIGMA, x, nnidx}
+
 A_values = measurement_matrix_values(mu, normals, SIGMA, x, nnidx);
 
 A = sparse(A_i(nnidx' > 0), ...
            A_j(nnidx' > 0), ...
-           A_values(:), ...
+           A_values(nnidx' > 0), ...
            n, p);
--- measurement_matrix_values.m
+++ measurement_matrix_values.m
@@ -1,4 +1,4 @@
-function A = measurement_matrix_values(mu, normals, SIGMA, x, nnidx)
+function A = measurement_matrix_values(mu, normals, SIGMA, x, nnidx) %#eml
 
 [n dim] = size(x);
 p_nn = size(nnidx,1);
@@ -7,21 +7,25 @@ A = zeros(n,p_nn);
 
 for i = 1:n
     neighborMuIndices = nnidx(:, i);
-    % The i'th measurement point might have less than size(nnidx, 1)
-    % neighbors. Eliminate empty idx entires
-    neighborMuIndices = neighborMuIndices(neighborMuIndices > 0);
     
     denominator = 0; % for normalization
     
-    j_ = 0;
+    j_ = 0; % Index into neighborMuIndices for the neighbor we're
+            % currently considering.
     
     for j=neighborMuIndices'
         j_ = j_ + 1;
-        kernel_value = gauss(x(i, :), mu(j, :), reshape(SIGMA(:,j,:,:), [dim dim]));
-        A(i, j_) = dot(normals(j,:), ...
-                                x(i,:) - mu(j,:), ...
-                                2) * kernel_value;
-        denominator = denominator + kernel_value;
+        
+        % The i'th measurement point might have less than size(nnidx, 1)
+        % neighbors. Spare entries in nnidx will be 0. Don't perform any
+        % computation for those.
+        if j > 0
+            kernel_value = gauss(x(i, :), mu(j, :), reshape(SIGMA(:,j,:,:), [dim dim]));
+            A(i, j_) = dot(normals(j,:), ...
+                          x(i,:) - mu(j,:), ...
+                          2) * kernel_value;
+            denominator = denominator + kernel_value;
+        end
     end
     A(i, :) = A(i, :) ./ denominator;
 end
