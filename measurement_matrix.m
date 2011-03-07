5ce4edf43aefb8da9a436c1d671081f443de4115
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-03-07 14:50:19 +0100

    Added comments

--- measurement_matrix.m
+++ measurement_matrix.m
@@ -1,19 +1,28 @@
 function A = measurement_matrix(mu, normals, SIGMA, x)
 
+%% Find nearest neighbors using ann library.
+% Save current path, so we can restore it later.
 old_path = path;
-addpath([pwd '/../ann_mwrapper'])
 
+% Change path, so that ann wrapper files are found.
+addpath([pwd '/../ann_mwrapper']) % Full path needed, so prepend working dir
+
+% Query for nearest 10 mu for each x.
 nnidx = annquery(mu', x', 10);
 
+% Clean up. (Restore previous path.)
 path(old_path)
 
+%% Determine sizes of input arguments
 n = size(x, 1);
 p = size(mu, 1);
 p_nn = size(nnidx, 1);
 
+%% Indices of measurement matrix' non-zero values
 A_i = repmat((1:n)', [1 p_nn]);
 A_j = double(nnidx');
 
+%% (Non-zero) values of measurement matrix
 % The sizes of passed parameters are not known in advance, so we compile
 % measurement_matrix_values just before calling it. For large n or p_nn
 % this still pays off.
@@ -21,6 +30,7 @@ emlmex measurement_matrix_values -eg {mu, normals, SIGMA, x, nnidx}
 
 A_values = measurement_matrix_values(mu, normals, SIGMA, x, nnidx);
 
+%% Compose measurement matrix
 A = sparse(A_i(nnidx' > 0), ...
            A_j(nnidx' > 0), ...
            A_values(nnidx' > 0), ...
