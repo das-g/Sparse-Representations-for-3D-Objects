4eedb4ed45758daf8e486479e7e165afd55b7a01
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-03-07 16:22:40 +0100

    Added comments about measurement_matrix_values compilation.

--- measurement_matrix.m
+++ measurement_matrix.m
@@ -35,16 +35,26 @@ A_i = repmat((1:n)', [1 p_nn]);
 A_j = double(nnidx');
 
 %% (Non-zero) values of measurement matrix
+% We call measurement_matrix_values to get the values. For large n and/or
+% large p_nn, it pays off to compile it, first. The caller can request
+% compilation by passing ..., 'compile', true.
+
 if ip.Results.compile
+    %% Caller requested compile
     % The sizes of passed parameters are not known in advance, so we compile
     % measurement_matrix_values just before calling it. For large n or p_nn
     % this still pays off.
     emlmex -o mmv_compiled measurement_matrix_values -eg {mu, normals, SIGMA, x, nnidx}
     
+    % Call compiled function.
     A_values = mmv_compiled(mu, normals, SIGMA, x, nnidx);
 else
+    %% Caller didn't request compile
+    % Make sure we'll call the *.m file, not a compiled function.
     assert(exist('measurement_matrix_values', 'file') == 2, ... Check whether *.m function.
            'Please delete the measurement_matrix_values MEX file.')
+    
+    % Call it.
     A_values = measurement_matrix_values(mu, normals, SIGMA, x, nnidx);
 end
 
