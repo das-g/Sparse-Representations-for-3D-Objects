a952c51d9cb13eb7edf8e89953e74205697e04d7
Raphael Borun Das Gupta <sparse-rep@raphael.dasgupta.ch>
2011-04-12 16:06:37 +0200

    iterate over a range of sigmas

--- main.m
+++ main.m
@@ -8,67 +8,73 @@ corners = [min(x); max(x)];
 center = mean(corners,1);
 corners = (corners - repmat(center,[2 1])) * 1.2 + repmat(center,[2 1]);
 
-start_sigma = 7;
+sigma_range = 1:0.3:15;
 
-% plot_f(x, x_normals, ...
-%        repmat(reshape(eye(2) * start_sigma^2, ...
-%                       [1 2 2]), ...
-%               [size(x,1) 1 1]), ...
-%        corners, ...
-%        'res', res)
-% hold
-% quiver(x(:,1),x(:,2),x_normals(:,1),x_normals(:,2),'color','black')
-% 
-% figure
+for i = 1:length(sigma_range)
 
-mu = x;
-SIGMA = repmat(reshape(eye(2) * start_sigma^2, ...
-                       [1 1 2 2]), ...
-                  [1 size(x,1) 1 1]);
+    start_sigma = sigma_range(i);
 
-% plot_f(mu, mu_normals, squeeze(SIGMA), corners, 'res',res)
-% hold
-% quiver(mu(:,1),mu(:,2),mu_normals(:,1),mu_normals(:,2),'color','black')
-% scatter(x(:,1),x(:,2),'.r')
+    % plot_f(x, x_normals, ...
+    %        repmat(reshape(eye(2) * start_sigma^2, ...
+    %                       [1 2 2]), ...
+    %               [size(x,1) 1 1]), ...
+    %        corners, ...
+    %        'res', res)
+    % hold
+    % quiver(x(:,1),x(:,2),x_normals(:,1),x_normals(:,2),'color','black')
+    % 
+    % figure
 
-% Query points MUST be measurement points, reference points MUST be kernel centers,
-% NOT the other way around. Else only the nearest k measurement points
-% would 'see' a given kernel.
-Xq = [x - x_normals * start_sigma * 0.5;
-      x;
-      x + x_normals * start_sigma * 0.5]; % query points
-%mu = x; % reference points, use input points for now
-mu_normals = x_normals; % normals at reference points (i.e. at kernel centers)
+    mu = x;
+    SIGMA = repmat(reshape(eye(2) * start_sigma^2, ...
+                           [1 1 2 2]), ...
+                      [1 size(x,1) 1 1]);
 
-[n dim] = size(Xq);
-p = size(mu,1);
+    % plot_f(mu, mu_normals, squeeze(SIGMA), corners, 'res',res)
+    % hold
+    % quiver(mu(:,1),mu(:,2),mu_normals(:,1),mu_normals(:,2),'color','black')
+    % scatter(x(:,1),x(:,2),'.r')
 
-A = measurement_matrix(mu, mu_normals, SIGMA, Xq);
+    % Query points MUST be measurement points, reference points MUST be kernel centers,
+    % NOT the other way around. Else only the nearest k measurement points
+    % would 'see' a given kernel.
+    Xq = [x - x_normals * start_sigma * 0.5;
+          x;
+          x + x_normals * start_sigma * 0.5]; % query points
+    %mu = x; % reference points, use input points for now
+    mu_normals = x_normals; % normals at reference points (i.e. at kernel centers)
 
-% right-hand side (measured f)
-rhs = weighted_signed_distance_fu( x, x_normals, ...
-                                   repmat(reshape(eye(2) * start_sigma^2, ...
-                                                  [1 dim dim]), ...
-                                   [p 1 1]), Xq );
-old_path = path;
-addpath([pwd '/../l1_ls_matlab'])
+    [n dim] = size(Xq);
+    p = size(mu,1);
 
-lambda = 0.01;
-[coeff_L1ls status] = l1_ls(A, rhs, lambda, 1e-3, true);
+    A = measurement_matrix(mu, mu_normals, SIGMA, Xq);
 
-path(old_path)
+    % right-hand side (measured f)
+    rhs = weighted_signed_distance_fu( x, x_normals, ...
+                                       repmat(reshape(eye(2) * start_sigma^2, ...
+                                                      [1 dim dim]), ...
+                                       [p 1 1]), Xq );
+    old_path = path;
+    addpath([pwd '/../l1_ls_matlab'])
 
-assert(all(status == 'Solved'))
+    lambda = 0.01;
+    [coeff_L1ls status] = l1_ls(A, rhs, lambda, 1e-3, true);
 
-% sparsify (eliminate almost-zero entries)
-threshold = 0.1;
-coeff_L1ls_nonzero_idx = abs(coeff_L1ls) > threshold; % vector of booleans
+    path(old_path)
 
-coeff_L1ls_reduced = coeff_L1ls(coeff_L1ls_nonzero_idx);
-mu_reduced = mu(coeff_L1ls_nonzero_idx, :);
-mu_normals_reduced = mu_normals(coeff_L1ls_nonzero_idx, :);
-SIGMA_reduced = SIGMA(:, coeff_L1ls_nonzero_idx, :, :);
+    assert(all(status == 'Solved'))
 
-% reconstruct the target function from the reduced data
-plot_approx(mu_reduced, mu_normals_reduced, SIGMA_reduced, ...
-            coeff_L1ls_reduced, corners, 'res', 200)
+    % sparsify (eliminate almost-zero entries)
+    threshold = 0.1;
+    coeff_L1ls_nonzero_idx = abs(coeff_L1ls) > threshold; % vector of booleans
+
+    coeff_L1ls_reduced = coeff_L1ls(coeff_L1ls_nonzero_idx);
+    mu_reduced = mu(coeff_L1ls_nonzero_idx, :);
+    mu_normals_reduced = mu_normals(coeff_L1ls_nonzero_idx, :);
+    SIGMA_reduced = SIGMA(:, coeff_L1ls_nonzero_idx, :, :);
+
+    % reconstruct the target function from the reduced data
+    plot_approx(mu_reduced, mu_normals_reduced, SIGMA_reduced, ...
+                coeff_L1ls_reduced, corners, 'res', 200)
+
+end
